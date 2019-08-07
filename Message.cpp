f1269dbfb535f8394a2146a30b3d0d054301c08a
amir hoze <31392918+amirhoze@users.noreply.github.com>
2019-08-07 14:35:38 +0300

    added Massage constructor

--- Message.cpp
+++ Message.cpp
@@ -6,5 +6,16 @@
 
 Message::Message(ifstream _file)
 {
+    getline(_file, start_line);
 
+    string line_str;
+    while (line_str[0] != 'CRLF');
+    {
+        getline(_file, line_str);
+        if (line_str.find("Content-Length:") != string::npos)
+        {
+            length = stoi(line_str);
+        }
+        number_of_headers++;
+    }
 }
